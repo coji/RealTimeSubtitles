57b276b1fb3a16f73cb4e58dd3581da37a1a113d
coji <coji@techtalk.jp>
2024-07-01 18:36:57 +0900

    Initial Commit

---
+++ RealTimeSubtitles/RealTimeSubtitlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimeSubtitlesApp.swift
+//  RealTimeSubtitles
+//
+//  Created by coji on 2024/07/01.
+//
+
+import SwiftUI
+
+@main
+struct RealTimeSubtitlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
